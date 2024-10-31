90cae87d11fa52129b5ce498fcf97294001ab114
petrovpe <petrovpe.ge@gmail.com>
2024-10-31 23:01:07 +0300

    UNP-1, UNP-23, UNP-20
    Added:
    - UI Main Menu and logic
    - UI Settings menu and
    - UI Video Settings menu and
    - Level loading

---
+++ Source/UnannouncedProject/GameLogic/MyGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "Blueprint/UserWidget.h"
+#include "MyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNANNOUNCEDPROJECT_API AMyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
